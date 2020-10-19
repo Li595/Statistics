1010975d132a62f30cf783cffac6ccd7d84bdec4
Li595 <61666020+Li595@users.noreply.github.com>
2020-10-19 15:32:42 +0300

    Update statisticsL.py

--- statisticsL.py
+++ statisticsL.py
@@ -1,19 +1,75 @@
+#Задание: Обработчик выборки 
+#Выполнили: Шишков Л., Язданов Р., Аполлова А.
+
 from openpyxl import load_workbook, Workbook
 import statistics
 
-# Выполнили: Шишков Л.А., Язданов Р.Н.
-
-class StatisticsL():        
-    def statisticV(name):                           #метод расчёта числовых характеристик выборки
-        dlina = len(name)                           #размер выборки
-        srednee = statistics.mean(name)             #среднее значение выборки
-        dispersia = statistics.variance(name)       #дисперсия выборки
-        sko = statistics.stdev(name)                #СКО выборки
-        try:                                        #мода выборки
+class StatisticsL():                                #класс рачёта числовых характеристик
+    def dlinaV(name):                               #метод расчёта длины выборки                         
+        dlina = len(name)
+        return('Длина выборки', dlina)
+    def sredneeV(name):                             #метод расчёта среднего значения выборки       
+        srednee = statistics.mean(name)
+        return("Среднее значение выборки", srednee)
+    def dispersiaV(name):                           #метод расчёта дисперсии выборки
+        dispersia = statistics.variance(name)
+        return("Дисперсия выборки", dispersia)
+    def skoV(name):                                 #метод расчёта СКО выборки        
+        sko = statistics.stdev(name)
+        return("СКО выборки", sko)
+    def modaV(name):                                 #метод расчёта моды выборки       
+        try:                                        
             moda = statistics.mode(name)
         except:
             moda = "-"
-        mediana = statistics.median(name)           #медиана выборки
-    def sortedV(name):                              #метод сортировки выборки
-        sort = sorted(name)                         #отсортированная выборка
+        return("Мода выборки", moda)
+    def medianaV(name):                             #метод расчёта медианы выборки        
+        mediana = statistics.median(name)
+        return("Медиана выборки", mediana)
+    def sortV(name):                                #метод сортировки выборки
+        sort = sorted(name)
+        return("Отсортированная выборка", sort)
+               
+#ПРОВЕРКА
+wb = load_workbook(str(input('Введите название файла ')))   #Открытие выборки
+sheet = wb.get_sheet_by_name('Лист1')
+temp = []
+temp2 = []
+for row in sheet.rows:
+	if type(row[0].value)!=str:
+		temp.append(float(row[0].value))
+		temp2.append(float(row[1].value))
 
+re = 1
+while re<=8:
+	re = int(input("""Действие:
+	1. Длина выборки
+	2. Среднее значение выборки
+	3. Дисперсия выборки
+	4. СКО выборки
+    5. Мода выборки
+    6. Медиана выборки
+    7. Отсортированная выборка
+	8. Выход
+	Выбор: """))
+	if re==1:
+		A = StatisticsL.dlinaV(temp)
+		print(A)
+	if re==2:
+		A = StatisticsL.sredneeV(temp)
+		print(A)
+	if re==3:
+		A = StatisticsL.dispersiaV(temp)										
+		print(A)
+	if re==4:
+		A = StatisticsL.skoV(temp)
+		print(A)
+	if re==5:
+		A = StatisticsL.modaV(temp)
+		print(A)       
+	if re==6:
+		A = StatisticsL.medianaV(temp)
+		print(A) 
+	if re==7:
+		A = StatisticsL.sortV(temp)
+		print(A)               
